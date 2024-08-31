c5a0d6ae3b7b00498be1acd6124235ce6be8dbd6
jonangjj <jonathanang.2022@scis.smu.edu.sg>
2024-08-31 16:49:27 +0800

    task mode update

--- backend/models/Task.js
+++ backend/models/Task.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    priority: { type: String, enum: ['High', 'Medium', 'Low'], required: true },
-    status: { type: String, enum: ['In Progress', 'Completed', 'Pending'], required: true },
-    date: { type: Date, required: true },
-    time: { type: String, required: true },
-    description: { type: String, required: true },
-    assignedTo: [{ type: String }], // Array of strings for assigned users
-    category: { type: String, required: true },
-    karmaPoints: { type: Number, required: true }
+    title: { type: String, required: true }, // Title of the task
+    description: { type: String, required: true }, // Description of the task
+    location: { type: String, required: true }, // Location of the task
+    type: { type: String, enum: ['Engagement', 'Event'], required: true }, // Type of task (Engagement, Event)
+    timing: { type: String, required: true }, // Timing details of the task
+    isOrganization: { type: Boolean, required: true }, // Boolean to indicate if it's an organization-led task
+    image: { type: String }, // URL or path to the task image
+    karmaPoints: {type: Number, default: 100} // set to 100 karma points for now
   });
 
 module.exports = mongoose.model('Task', TaskSchema);
